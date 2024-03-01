6d3dd27e7c62802b2303f3096befea85ed5a9f65
wrayjaehwa <jaehwawray@gmail.com>
2024-03-01 17:51:39 -0500

    header component test

--- book-bazaar/src/App.js
+++ book-bazaar/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css'; // Import CSS file for styling
+import Title from './Title'; // Title component
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 // marko's git commit test
 // practice
@@ -10,18 +11,7 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 function App() {
   return (
     <div className="app">
-      <div className="header">
-        <div className="logo">
-          <img src="/book_bazaar_logo.png" alt="Logo" />
-        </div>
-        <div className="search-bar">
-          <input type="text" placeholder="Search ISBN..." />
-          <button>
-            {/* <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> */}
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-        </div>
-      </div>
+      <Title />
       <div className="body">
         <div className="sidebar">
           {/* Left Sidebar */}
