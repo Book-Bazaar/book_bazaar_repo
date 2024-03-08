255775136b15b61d53bae2cc6f3cebcd4fe09309
Marko Drazic <markodrazic757@gmail.com>
2024-03-08 13:28:58 -0500

    bubble v1

--- book-bazaar/src/Pages/login.jsx
+++ book-bazaar/src/Pages/login.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-// import { useForm, SumbitHandler } from 'react-hook-form';
+import './login.css';
+
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -31,30 +32,49 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleSubmit} className="login-form">
-        <input
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" className="login-button">
-          Login
-        </button>
-      </form>
-      <p>
-        <Link to="/signup">Need to create an account?</Link>
-      </p>
+    <div class="container">
+      <div class="drop">
+        <div class="content">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit} className="login-form">
+            
+            {/* INPUT FOR EMAIL */}
+            <div class="inputBox">
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            {/* INPUT FOR PASSWORD */}
+            <div class="inputBox">
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            {/* LOGIN BUTTON */}
+            <div class="inputBox">
+              <button type="submit" className="login-button">
+                Login
+              </button>
+            </div>
+          </form>
+
+          {/* SIGNUP LINK */}
+          <div class="btns">
+            <p> <Link to="/signup">SignUp</Link> </p>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
