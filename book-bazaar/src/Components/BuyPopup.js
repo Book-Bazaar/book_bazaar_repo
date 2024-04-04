e7a5882fc3a2934042a6251297fe484a79a26de6
Jae Hwa <jwray2@pride.hofstra.edu>
2024-04-04 18:26:55 -0400

    added Delete listings on marketplace tiles

--- book-bazaar/src/Components/BuyPopup.js
+++ book-bazaar/src/Components/BuyPopup.js
@@ -3,10 +3,10 @@ import './BuyPopup.css';
 import {collection, deleteDoc, getDocs, doc, getFirestore, getDoc} from 'firebase/firestore';
 import {firestore, colRefBooks, auth} from '../firebase';
 
+
 const Popup = ({ entry, onClose }) => {
   const { imgurl, price, title, author, condition, summary, id, email } = entry;
-
-  // condition tag
+  
   let conditionClass;
   switch (condition) {
     case 'new':
@@ -24,18 +24,16 @@ const Popup = ({ entry, onClose }) => {
 
   const handleContactSeller = async () => {
     try {
-      var base = 'mailto:' + email;
-      var subject = '?subject=Offer from Book Bazaar';
-      var msg =
+      const base = 'mailto:' + email;
+      const subject = '?subject=Offer from Book Bazaar';
+      const msg =
         "&body=I'm interested in buying your copy of " +
         title +
         ' via Book Bazaar!';
 
       const docSnapshot = await getDoc(doc(firestore, 'Books', id));
 
-      // deleting book from firebase
       if (docSnapshot.exists()) {
-        // delete doc
         await deleteDoc(doc(firestore, 'Books', id));
         window.open(base + subject + msg);
         console.log('Entry deleted successfully');
@@ -44,7 +42,24 @@ const Popup = ({ entry, onClose }) => {
         }, 1800);
       } else {
         console.log('Document does not exist in the Firestore database');
-        
+      }
+    } catch (error) {
+      console.error('Error deleting entry:', error);
+    }
+  };
+
+  const handleDeleteListing = async () => {
+    try {
+      const docSnapshot = await getDoc(doc(firestore, 'Books', id));
+
+      if (docSnapshot.exists()) {
+        await deleteDoc(doc(firestore, 'Books', id));
+        console.log('Entry deleted successfully');
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+      } else {
+        console.log('Document does not exist in the Firestore database');
       }
     } catch (error) {
       console.error('Error deleting entry:', error);
@@ -73,7 +88,12 @@ const Popup = ({ entry, onClose }) => {
               readOnly={true}
               style={{ fontFamily: 'inherit', fontSize: 'inherit' }}
             ></textarea>
-            <button className="contact-button" onClick={handleContactSeller}>Contact Seller</button>
+            {auth.currentUser.email === email && (
+                <button background-color="red" onClick={handleDeleteListing} >Delete Listing</button>
+            )}
+            {!auth.currentUser || auth.currentUser.email !== email && (
+              <button className="contact-button" onClick={handleContactSeller}>Contact Seller</button>
+            )}
             <button onClick={onClose}>Close</button>
           </div>
         </div>
