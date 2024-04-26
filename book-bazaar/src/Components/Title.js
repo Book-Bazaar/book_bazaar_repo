329b2024345e1fc3bc23e9b37dac1973e50177c2
Rutvikshah14 <rutvikshah979@gmail.com>
2024-04-26 04:41:33 -0400

    Dark Mode 2.0 - Title Done

--- book-bazaar/src/Components/Title.js
+++ book-bazaar/src/Components/Title.js
@@ -1,7 +1,9 @@
 // src/Title.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Title.css';
+import './darkMode/TitleDarkMode.css'; // Import dark mode styles
+import './darkMode/darkModeBtn.css'
 import SearchBar from './SearchBar';
 import { Profile } from './Profile';
 // import { Slbuttons } from './Slbuttons.js';
@@ -9,16 +11,43 @@ import logo from '../Assets/book_bazaar_logo.png';
 import { Link } from 'react-router-dom'; // Import Link component for routing
 
 function Title({ setSearchQuery }) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    // Save the user preference in local storage
+    localStorage.setItem('darkMode', !darkMode);
+    window.location.reload();
+  };
+
+  // Load the user preference from local storage on component mount
+  useState(() => {
+    const userDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+    if (userDarkMode !== null) {
+      setDarkMode(userDarkMode);
+    }
+  }, []);
   // console.log({ setResults });
   //use results to effect marketplace showing
   return (
-    <div className="Title">
+    <div className={`Title ${darkMode ? 'dark-mode' : ''}`}>
       {/* Logo space */}
       <Link to="/">
         <img src={logo} alt="" className="logo" />
       </Link>
       {/* Search bar space */}
       <SearchBar setSearchQuery={setSearchQuery} />
+
+      <label class='txt'>
+          <b>
+            {darkMode ? 'Dark' : 'Light'}
+        </b>
+      </label>
+      <label className="switch">
+        <input type="checkbox" checked={darkMode} onChange={toggleDarkMode}></input>
+        <span className="slider round"></span>
+      </label>
+      
 
       <Profile />
     </div>
